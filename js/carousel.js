1b127b2d76407dc7b1bef27d4f6cfd641e85242e
Osman <102681175+KingZxi@users.noreply.github.com>
2024-02-27 15:37:33 +0000

    Added jQuery and Slick plugin, worked on a slideshow banner which is functional but can still use some work

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,15 @@
 $(".slick-banner").slick({
-  dots: false,
+  dots: true,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
   prevArrow: false,
   nextArrow: false,
+});
+
+const dots = document.querySelector(".slick-dots");
+
+Array.from(dots.children).forEach((child) => {
+  const span = document.createElement("span");
+  child.appendChild(span);
 });
