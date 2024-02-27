9e5ed59f75ff059abbb53af581d7af07de52984d
Osman <102681175+KingZxi@users.noreply.github.com>
2024-02-27 09:01:56 +0000

    Many changes and implementations, in short I have worked on a sticky-header, sidebar and mobile-exclusive nav menu, I also added the prompt to suggest accepting cookies to the user, the response to that prompt is saved within the browser cookies and read on reload

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
+const hambutton = document.querySelector(".hambutton");
 const sidebar = document.querySelector(".sidebar");
 const main = document.querySelector(".main-content");
 
 hamburger.addEventListener("click", () => {
   sidebar.classList.toggle("sidebar-reveal");
   main.classList.toggle("sidebar-push");
+
+  hambutton.classList.toggle("open");
 });
