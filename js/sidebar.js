579af8259ab3673b1f7e1651d57ec8e9cba5f991
Osman <102681175+KingZxi@users.noreply.github.com>
2024-02-27 16:45:32 +0000

    Fixed clone header not responding to interaction

--- js/sidebar.js
+++ js/sidebar.js
@@ -6,6 +6,17 @@ const main = document.querySelector(".main-content");
 hamburger.addEventListener("click", () => {
   sidebar.classList.toggle("sidebar-reveal");
   main.classList.toggle("sidebar-push");
-
   hambutton.classList.toggle("open");
+  hambuttonC.classList.toggle("open");
+});
+
+//Everything below here and also the hamburgerC toggle above is used for the clone header, I may change how this works later
+const hamburgerC = document.querySelectorAll(".hamburger")[1];
+const hambuttonC = document.querySelectorAll(".hambutton")[1];
+
+hamburgerC.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar-reveal");
+  main.classList.toggle("sidebar-push");
+  hambutton.classList.toggle("open");
+  hambuttonC.classList.toggle("open");
 });
