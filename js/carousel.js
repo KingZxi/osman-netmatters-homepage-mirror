99a2080c7d0cdfdbebb006770064740023fd9c3e
Osman <102681175+KingZxi@users.noreply.github.com>
2024-02-27 16:36:58 +0000

    Added autoplay to the banner

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,6 @@
 $(".slick-banner").slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
   dots: true,
   infinite: true,
   speed: 300,
@@ -7,14 +9,17 @@ $(".slick-banner").slick({
   nextArrow: false,
 });
 
+//Selecting dots container and buttons
 const dots = document.querySelector(".slick-dots");
 const buttons = document.querySelectorAll(".slick-dots li button");
 
+//Creating a span within each button, this span is used to apply the dot around active items
 Array.from(dots.children).forEach((child) => {
   const span = document.createElement("span");
   child.appendChild(span);
 });
 
+//For each loop that clears the content within each button (they are numbered which causes added spacing)
 buttons.forEach((button) => {
   console.log(button);
   button.textContent = " ";
