c378e3369cbdf0e30ea85e72780b99c9eb088f1d
Osman <102681175+KingZxi@users.noreply.github.com>
2024-02-27 14:02:12 +0000

    Restructured how the banner displays the background images for some time, before reverting to original solution

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,8 @@
-// $(".banner").slick({
-//   dots: true,
-//   arrows: false,
-// });
+$(".slick-banner").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  prevArrow: false,
+  nextArrow: false,
+});
