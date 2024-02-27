f6815a684443b53dd169d5ebc6dbd09cc9d59089
Osman <102681175+KingZxi@users.noreply.github.com>
2024-02-27 16:33:17 +0000

    Polishing banner dots

--- js/carousel.js
+++ js/carousel.js
@@ -8,8 +8,14 @@ $(".slick-banner").slick({
 });
 
 const dots = document.querySelector(".slick-dots");
+const buttons = document.querySelectorAll(".slick-dots li button");
 
 Array.from(dots.children).forEach((child) => {
   const span = document.createElement("span");
   child.appendChild(span);
+});
+
+buttons.forEach((button) => {
+  console.log(button);
+  button.textContent = " ";
 });
