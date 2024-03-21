7929c8991dbcee75a47d8cd3c32e33e6cae10355
Osman <102681175+KingZxi@users.noreply.github.com>
2024-03-21 14:52:14 +0000

    Modified the way the cookies prompt displays, should appear more cleanly now

--- js/cookies.js
+++ js/cookies.js
@@ -1,13 +1,12 @@
 const accept = document.querySelector(".btn-accept");
 const cookies = document.querySelector(".cookies");
 
-
 accept.addEventListener("click", () => {
-  cookies.classList.toggle("cookies-hide");
+  cookies.classList.toggle("cookies-show");
   document.cookie = "cookies=true";
 });
 
 //Checking to see if user has accepted cookies before
-if (document.cookie.includes("cookies=true")) {
-  cookies.classList.toggle("cookies-hide");
+if (!document.cookie.includes("cookies=true")) {
+  cookies.classList.toggle("cookies-show");
 }
