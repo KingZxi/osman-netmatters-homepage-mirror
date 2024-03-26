9edbe7e443fc38ab8daec44720747742718b4522
Osman <102681175+KingZxi@users.noreply.github.com>
2024-03-26 16:42:45 +0000

    Started working on the 'Our Office' location cards'

---
+++ contact.php
@@ -0,0 +1,63 @@
+<?php include 'includes/header.php' ?>
+
+
+<section class="breadcrums__section">
+</section>
+<div class="office__heading">
+    <h2 class="h1">Our Offices</h1>
+</div>
+<section class="office__section container">
+    <div class="office__flex">
+        <div class="office__item">
+            <img src="img/netmatter53.jpg">
+            <div class="office__content">
+                <h2><a>
+                        Cambridge Office
+                    </a></h2>
+                <p class="address">
+                    Unit 1.31,
+                    <br>
+                    St John's Innovation Centre,
+                    <br>
+                    Cowley Road,
+                    Milton,
+                    <br>
+                    Cambridge,
+                    <br>
+                    CB4 0WS
+                </p>
+                <p class="number">02045397354</p>
+                <a class="office__btn">VIEW MORE <em class="arrow-right"></em></a>
+            </div>
+            <div class="office__map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4947.572124758385!2d-0.008341052166301962!3d51.68205893741378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fff7296cdfb%3A0xfec3b5bbc572b29b!2sBrooker%20Rd%2C%20Waltham%20Abbey%20EN9%201JH!5e0!3m2!1sen!2suk!4v1711469644078!5m2!1sen!2suk"
+                    width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+
+</section>
+<section class="contact__section">
+</section>
+<?php include "includes/footer.php" ?>
+</div>
+
+<!-- Jquery Import -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<!-- Slick import -->
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js">
+</script>
+<!-- JS imports -->
+<script src="owlcarousel/owl.carousel.min.js"></script>
+<script src="js/carousel-slideshow.js"></script>
+<script src="js/carousel-banner.js"></script>
+<script src="js/sticky-header.js"></script>
+<script src="js/sidebar.js"></script>
+<script src="js/cookies.js"></script>
+
+</body>
+
+</html>
--- php/fetch-news.php
+++ php/fetch-news.php
@@ -16,13 +16,22 @@ try {
 
     //Looping through the news card table
     for ($i = 1; $i <= 3; $i++) {
-        $sql = "SELECT `heading`, `paragraph`, `tag`, `type`, `publisher`, `date_published`, `news_img`, `publisher_img` 
+        $sql = "SELECT `heading`, `paragraph`, `tag`, `type`, `publisher`, `date_published`, `news_img`, `publisher_img`, `read_time`
         FROM news 
         WHERE `news_id` = :news_id";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':news_id', $i);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //Changing date format:
+        $date_published = date('j F Y', strtotime($row['date_published']));
+        
+        //Adding read-time if value exists (prepending it with a hython)
+        $read_time = '';
+        if($row['read_time'] != null) {
+            $read_time = '- '. $row['read_time'];
+        }
 
         //echo'ing the card
         echo '
@@ -37,7 +46,12 @@ try {
                 <h3 class="news__card__info__heading">
                     <span class="news__card__info__h3">'.$row['heading'].'
                     </span>
-                    <span class="news__card__info__time"></span>
+                    <span class="news__card__info__time">'
+
+                    
+                    .$read_time.
+                    
+                    '</span>
                 </h3>
                 <p>
                     '.$row['paragraph'].'
@@ -47,7 +61,7 @@ try {
                     <img src="'.$row['publisher_img'].'" alt="Netmatters user profile picture" />
                     <div class="news__card__creator__info">
                         <strong>Posted by '.$row['publisher'].'</strong> <br />
-                        '.$row['date_published'].'
+                        '.$date_published.'
                     </div>
                 </div>
             </div>
