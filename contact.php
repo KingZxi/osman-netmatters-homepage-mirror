1a9636ea4a61973ec178a224017af8fe9791410e
Osman <102681175+KingZxi@users.noreply.github.com>
2024-03-26 21:57:49 +0000

    Working on contact section, added some sass

--- contact.php
+++ contact.php
@@ -9,6 +9,35 @@
 <section class="office__section container">
     <div class="office__flex">
         <div class="office__item">
+            <img src="img/netmatter52.jpg">
+            <div class="office__content">
+                <h2><a>
+                        London Office
+                    </a></h2>
+                <p class="address">
+                    Unit G6,
+                    <br>
+                    Pixel Business Centre,
+                    <br>
+                    110 Brooker Road,
+                    Waltham Abbey,
+                    <br>
+                    London,
+                    <br>
+                    EN9 1JH
+
+                </p>
+                <p class="number">02045397354</p>
+                <a class="office__btn">VIEW MORE <em class="arrow-right"></em></a>
+            </div>
+            <div class="office__map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4947.572124758385!2d-0.008341052166301962!3d51.68205893741378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fff7296cdfb%3A0xfec3b5bbc572b29b!2sBrooker%20Rd%2C%20Waltham%20Abbey%20EN9%201JH!5e0!3m2!1sen!2suk!4v1711469644078!5m2!1sen!2suk"
+                    width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        <div class="office__item">
             <img src="img/netmatter53.jpg">
             <div class="office__content">
                 <h2><a>
@@ -26,7 +55,65 @@
                     <br>
                     CB4 0WS
                 </p>
-                <p class="number">02045397354</p>
+                <p class="number">01223 37 57 72</p>
+                <a class="office__btn">VIEW MORE <em class="arrow-right"></em></a>
+            </div>
+            <div class="office__map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4947.572124758385!2d-0.008341052166301962!3d51.68205893741378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fff7296cdfb%3A0xfec3b5bbc572b29b!2sBrooker%20Rd%2C%20Waltham%20Abbey%20EN9%201JH!5e0!3m2!1sen!2suk!4v1711469644078!5m2!1sen!2suk"
+                    width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        <div class="office__item">
+            <img src="img/netmatter54.jpg">
+            <div class="office__content">
+                <h2><a>
+                        Wymondham Office
+                    </a></h2>
+                <p class="address">
+
+                    Unit 15,
+                    <br>
+                    Penfold Drive,
+                    <br>
+                    Gateway 11 Business Park,
+                    <br>
+                    Wymondham, Norfolk,
+                    <br>
+                    NR18 0WZ
+
+                </p>
+                <p class="number">01603 70 40 20</p>
+                <a class="office__btn">VIEW MORE <em class="arrow-right"></em></a>
+            </div>
+            <div class="office__map">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4947.572124758385!2d-0.008341052166301962!3d51.68205893741378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fff7296cdfb%3A0xfec3b5bbc572b29b!2sBrooker%20Rd%2C%20Waltham%20Abbey%20EN9%201JH!5e0!3m2!1sen!2suk!4v1711469644078!5m2!1sen!2suk"
+                    width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        <div class="office__item">
+            <img src="img/netmatter55.jpg">
+            <div class="office__content">
+                <h2><a>
+                        Great Yarmouth Office
+                    </a></h2>
+                <p class="address">
+
+                    Suite F23,
+                    <br>
+                    Beacon Innovation Centre,
+                    <br>
+                    Beacon Park, Gorleston,
+                    <br>
+                    Great Yarmouth, Norfolk,
+                    <br>
+                    NR31 7RA
+
+                </p>
+                <p class="number">01493 60 32 04</p>
                 <a class="office__btn">VIEW MORE <em class="arrow-right"></em></a>
             </div>
             <div class="office__map">
@@ -39,14 +126,59 @@
     </div>
 
 </section>
-<section class="contact__section">
+<section class="contact__section container">
+    <div class="contact__top">
+        <p class="contact__text">
+            Email us on:
+        </p>
+        <p class="contact__email">
+            sales@netmatters.com
+        </p>
+        <p class="contact__text">
+            Business hours:
+        </p>
+        <p class="contact__text">
+            Monday - Friday 07:00 - 18:00
+        </p>
+        <div>
+            <p class="contact__text">
+                Out of Hours IT Support:
+            </p>
+        </div>
+    </div>
+    <div class="contact__form">
+        <form action="submit.php" method="POST">
+            <div class="form-group">
+                <label for="name">Your Name</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="company">Company Name</label>
+                <input type="text" id="company" name="company" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Your Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="telephone">Your Telephone</label>
+                <input type="tel" id="telephone" name="telephone" required>
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea id="message" name="message" required></textarea>
+            </div>
+            <button type="submit" class="btn">Submit</button>
+        </form>
+    </div>
 </section>
 <?php include "includes/footer.php" ?>
 </div>
 
 <!-- Jquery Import -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"
-    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src=" https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous">
+</script>
 <!-- Slick import -->
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js">
 </script>
